588d34005625427d1ddb454703984018b9e5e4ce
rroy11705 <rroy11705@gmail.com>
2025-04-04 23:30:54 +0530

    feat: vercel storage blob

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,7 +1,9 @@
 import { Icon as Icon_0587cb67c1c8f60de0104817fb4e38c3 } from '../../../graphics/Icon/index.tsx'
 import { Logo as Logo_f636bbb7a5faa6d1a4eae8e6abdcedaf } from '../../../graphics/Logo/index.tsx'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 
 export const importMap = {
   "./graphics/Icon/index.tsx#Icon": Icon_0587cb67c1c8f60de0104817fb4e38c3,
-  "./graphics/Logo/index.tsx#Logo": Logo_f636bbb7a5faa6d1a4eae8e6abdcedaf
+  "./graphics/Logo/index.tsx#Logo": Logo_f636bbb7a5faa6d1a4eae8e6abdcedaf,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e
 }
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,5 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -10,6 +9,7 @@ import sharp from 'sharp'
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 import { Pages } from './collections/Pages'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -61,7 +61,12 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      collections: {
+        media: true,
+      },
+      addRandomSuffix: true,
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
 })
